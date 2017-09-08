c6e094fc33a0da0d7f10119af0022333ae653ce3
karthik <karthik.bits13@gmail.com>
2017-09-07 23:15:46 -0500

    Mock-api-json-data and some other stuff

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -15,6 +15,14 @@ app.use(require("webpack-dev-middleware")(compiler,{
     publicPath: config.output.publicPath
 }));
 
+app.get('/users', function(req, res){
+res.json([
+        {"id":1, "firstName":"Prathyusha", "lastName":"vempati", "email" : "rayalaprathusha@gmail.com"},
+        {"id":2, "firstName":"Pravasti", "lastName":"vempati", "email" : "pravastiv@gmail.com"},
+        {"id":3, "firstName":"Karthik", "lastName":"vempati", "email" : "karthik.bits13@gmail.com"}
+    ]);
+});
+
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
